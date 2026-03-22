427b6c356ef969cd643ff303e16ff392ad119903
yaara <yaara3332@gmail.com>
2026-03-22 19:28:39 +0200

    commit3

--- app/src/main/java/com/example/demo_12/MainActivity.java
+++ app/src/main/java/com/example/demo_12/MainActivity.java
@@ -14,3 +14,27 @@ public class MainActivity extends AppCompatActivity {
     public ToggleButton toggleButton;
     public ConstraintLayout constraintLayout;
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        constraintLayout = findViewById(R.id.main);
+        aSwitch = findViewById(R.id.switch1);
+        toggleButton = findViewById(R.id.toggle);
+        aSwitch.setChecked(true);
+    }
+
+    public void clicked(View view) {
+        if (toggleButton.isChecked() && aSwitch.isChecked()) {
+            constraintLayout.setBackgroundColor(Color.RED);
+        } if (toggleButton.isChecked() && !aSwitch.isChecked()) {
+            constraintLayout.setBackgroundColor(Color.BLUE);
+        } if (!aSwitch.isChecked() && !toggleButton.isChecked()) {
+            constraintLayout.setBackgroundColor(Color.MAGENTA);
+        } if (!toggleButton.isChecked() && aSwitch.isChecked()) {
+            constraintLayout.setBackgroundColor(Color.GREEN);
+        }
+    }
+}
