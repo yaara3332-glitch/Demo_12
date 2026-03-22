c12f2200a2c25dc2bc894867e4d20d589bdf61e7
yaara <yaara3332@gmail.com>
2026-03-22 19:26:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/demo_12/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.demo_12;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.ToggleButton;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class MainActivity extends AppCompatActivity {
+    public Switch aSwitch;
+    public ToggleButton toggleButton;
+    public ConstraintLayout constraintLayout;
+
